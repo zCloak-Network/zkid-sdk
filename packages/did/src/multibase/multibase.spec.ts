1607825791ad0e5e2fdcd9c31d072a34ac54b8b3
Zhichao Zhang <zzcwoshizz@163.com>
2022-10-17 18:54:19 +0800

    add test

--- packages/did/src/multibase/multibase.spec.ts
+++ packages/did/src/multibase/multibase.spec.ts
@@ -1,4 +1,6 @@
-import { base58Decode, base58Encode } from './bs58';
+import { base32Decode, base32Encode, base32Validate } from './bs32';
+import { base58Decode, base58Encode, base58Validate } from './bs58';
+import { base64Decode, base64Encode, base64Validate } from './bs64';
 
 describe('multibase', (): void => {
   it('base58 encode', (): void => {
@@ -10,5 +12,36 @@ describe('multibase', (): void => {
     const bytes = base58Decode('z3VNr6P');
 
     expect(new TextDecoder().decode(bytes)).toEqual('abcd');
+  });
+  it('base58 validate', (): void => {
+    base58Validate('z3VNr6P');
+  });
+
+  it('base32 encode', (): void => {
+    const bytes = new TextEncoder().encode('abcd');
+
+    expect(base32Encode(bytes)).toEqual('bmfrggza');
+  });
+  it('base32 decode', (): void => {
+    const bytes = base32Decode('bmfrggza');
+
+    expect(new TextDecoder().decode(bytes)).toEqual('abcd');
+  });
+  it('base32 validate', (): void => {
+    base32Validate('bmfrggza');
+  });
+
+  it('base64 encode', (): void => {
+    const bytes = new TextEncoder().encode('abcd');
+
+    expect(base64Encode(bytes)).toEqual('mYWJjZA==');
+  });
+  it('base64 decode', (): void => {
+    const bytes = base64Decode('mYWJjZA==');
+
+    expect(new TextDecoder().decode(bytes)).toEqual('abcd');
+  });
+  it('base64 validate', (): void => {
+    base64Validate('mYWJjZA==');
   });
 });

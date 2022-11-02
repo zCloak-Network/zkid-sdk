627b25d71847a35fd3e8caad80d1e05edeb935ec
zzcwoshizz <zzcwoshizz@163.com>
2022-11-02 15:30:24 +0800

    vc packages (#11)
    * add @zcloak/util package
    
    * packages: @zcloak/vc @zcloak/verify
    
    * remove util package
    
    * fix lint

--- jest.config.cjs
+++ jest.config.cjs
@@ -6,7 +6,9 @@ module.exports = {
     '@zcloak/crypto(.*)$': '<rootDir>/packages/crypto/src/$1',
     '@zcloak/did-resolver(.*)$': '<rootDir>/packages/did-resolver/src/$1',
     '@zcloak/did(.*)$': '<rootDir>/packages/did/src/$1',
-    '@zcloak/keyring(.*)$': '<rootDir>/packages/keyring/src/$1'
+    '@zcloak/keyring(.*)$': '<rootDir>/packages/keyring/src/$1',
+    '@zcloak/vc(.*)$': '<rootDir>/packages/vc/src/$1',
+    '@zcloak/verify(.*)$': '<rootDir>/packages/verify/src/$1'
   },
   testTimeout: 30000
 };

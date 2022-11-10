00e53d6bd7b4e2780e88a6c658b2aad3550485eb
Zhichao Zhang <zzcwoshizz@163.com>
2022-11-10 23:31:56 +0800

    fix build lint

--- packages/crypto/src/initCrypto.ts
+++ packages/crypto/src/initCrypto.ts
@@ -1,7 +1,6 @@
 // Copyright 2021-2022 zcloak authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import {} from '@polkadot/wasm-crypto/initOnlyWasm';
 import { initWasm as initPolkadotWasm } from '@polkadot/wasm-crypto/initWasmAsm';
 
 import { initWasm } from '@zcloak/wasm';

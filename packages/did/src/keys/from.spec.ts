2306e5ff3e32928dd9e8c1baffcc05a6f87da1f8
Zhichao Zhang <zzcwoshizz@163.com>
2023-02-23 16:53:53 +0800

    add unit test

---
+++ packages/did/src/keys/from.spec.ts
@@ -0,0 +1,37 @@
+// Copyright 2021-2023 zcloak authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+import { DOCUMENTS, testKeyring } from 'test-support';
+
+import { ethereumEncode, initCrypto } from '@zcloak/crypto';
+
+import { fromMnemonic } from './from';
+
+describe('create did use keys', (): void => {
+  beforeAll(async () => {
+    await initCrypto();
+  });
+
+  it('fromMnemonic with index 0 and type ecdsa', () => {
+    const mnemonic =
+      'health correct setup usage father decorate curious copper sorry recycle skin equal';
+    const did = fromMnemonic(testKeyring, mnemonic, 'ecdsa', 0);
+
+    expect(did.getDocument()).toMatchObject(DOCUMENTS.alice);
+  });
+
+  it.each(Array.from({ length: 10 }).map((_, index) => index))(
+    'fromMnemonic with index: %p',
+    (index) => {
+      const mnemonic =
+        'health correct setup usage father decorate curious copper sorry recycle skin equal';
+      const did = fromMnemonic(testKeyring, mnemonic, 'ecdsa', index);
+
+      expect(did.identifier).toBe(
+        ethereumEncode(
+          testKeyring.createFromMnemonic(mnemonic, `/m/44'/60'/0'/0/${index}`, 'ecdsa').publicKey
+        )
+      );
+    }
+  );
+});

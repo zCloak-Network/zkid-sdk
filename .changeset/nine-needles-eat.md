dee583c310db267baa744eece9802f3ccc384d1d
zzcwoshizz <zzcwoshizz@163.com>
2023-03-18 22:51:00 +0800

    calc recovery param, support signature when use ethereum sign. See more: https://eips.ethereum.org/EIPS/eip-155 (#64)

---
+++ .changeset/nine-needles-eat.md
@@ -0,0 +1,5 @@
+---
+"@zcloak/crypto": patch
+---
+
+calc recovery param, support signature when use ethereum sign. See more: https://eips.ethereum.org/EIPS/eip-155
--- packages/crypto/src/secp256k1/recover.ts
+++ packages/crypto/src/secp256k1/recover.ts
@@ -6,6 +6,16 @@ import type { HexString } from '@polkadot/util/types';
 import { recoverPublicKey, Signature } from '@noble/secp256k1';
 import { u8aToU8a } from '@polkadot/util';
 
+function calculateSigRecovery(recovery: number, chainId?: number): number {
+  if (recovery === 0 || recovery === 1) return recovery;
+
+  if (chainId === undefined) {
+    return recovery - 27;
+  }
+
+  return recovery - (chainId * 2 + 35);
+}
+
 /**
  * @name secp256k1Recover
  * @description Recovers a publicKey from the supplied signature
@@ -17,7 +27,8 @@ export function secp256k1Recover(
 ): Uint8Array {
   const sig = u8aToU8a(signature).subarray(0, 64);
   const msg = u8aToU8a(msgHash);
-  const publicKey = recoverPublicKey(msg, Signature.fromCompact(sig).toRawBytes(), recovery);
+
+  const publicKey = recoverPublicKey(msg, Signature.fromCompact(sig).toRawBytes(), calculateSigRecovery(recovery));
 
   if (!publicKey) {
     throw new Error('Unable to recover publicKey from signature');

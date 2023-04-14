4c3196a955258c8687fe71baa34486d53770fb64
vsszhang <vsszhang@gmail.com>
2023-04-14 14:04:15 +0800

    refactor: extract `createVC` logic

--- packages/cli/src/commands/issueVC.ts
+++ packages/cli/src/commands/issueVC.ts
@@ -4,15 +4,13 @@
 import fs from 'fs';
 
 import { initCrypto } from '@zcloak/crypto';
-import { CType } from '@zcloak/ctype/types';
 import { Did } from '@zcloak/did';
 import { fromDidDocument } from '@zcloak/did/did/helpers';
 import { DidDocument, DidUrl } from '@zcloak/did-resolver/types';
 import { encryptMessage } from '@zcloak/message';
-import { Raw, VerifiableCredentialBuilder } from '@zcloak/vc';
-import { HashType, RawCredential } from '@zcloak/vc/types';
+import { VerifiableCredential } from '@zcloak/vc/types';
 
-import { getCType, getDidDoc, getDidFromMnemonic, sendEncryptedMessage } from '../utils';
+import { createVC, getDidDoc, getDidFromMnemonic, sendEncryptedMessage } from '../utils';
 
 export const issueVC = async (
   env: string,
@@ -23,11 +21,12 @@ export const issueVC = async (
   rawHashType?: string,
   rawCredHashType?: string,
   isPublic?: number,
+  claimers?: string | undefined,
   output?: string
 ) => {
   let baseUrl: string;
 
-  if (claimerDid === undefined || ctypeHash === undefined || content === undefined) {
+  if (claimerDid === undefined || ctypeHash === undefined || content === undefined || isPublic === undefined) {
     console.log('your flag has undefined value');
 
     return;
@@ -56,41 +55,18 @@ export const issueVC = async (
   const mnemonicObj = JSON.parse(mnemonicContent);
   const attester: Did = getDidFromMnemonic(mnemonicObj.mnemonic);
 
-  // step 1: get ctype
-  const ctype: CType = await getCType(baseUrl, ctypeHash);
-
-  // step 2: build raw
-  const realRawHashType = rawHashType as HashType;
-  const rawContentContent = fs.readFileSync(content, { encoding: 'utf-8' });
-  const rawContent = JSON.parse(rawContentContent);
-  const raw = new Raw({
-    contents: rawContent,
-    owner: claimerDidUrl,
-    ctype,
-    hashType: realRawHashType
-  });
-
-  // step 3: build rawCredential from raw
-  const realRawCredHashType = rawCredHashType as HashType;
-  const rawCredential: RawCredential = raw.toRawCredential(realRawCredHashType);
-
-  // step 4: build a vcBuilder by using rawCredential and ctype
-  const vcBuilder = VerifiableCredentialBuilder.fromRawCredential(rawCredential, ctype)
-    .setExpirationDate(null)
-    .setIssuanceDate(Date.now());
-
-  // step5: build a vc
-  let vc;
-
-  if (isPublic === 0) {
-    vc = await vcBuilder.build(attester, false);
-  } else if (isPublic === 1) {
-    vc = await vcBuilder.build(attester, true);
-  } else {
-    console.log('invalid is-public flag value');
-
-    return;
-  }
+  // step 2: create VC
+  const rawContent = fs.readFileSync(content, { encoding: 'utf-8' });
+  const vc: VerifiableCredential<false | true> = await createVC(
+    baseUrl,
+    attester,
+    claimerDid,
+    ctypeHash,
+    rawContent,
+    isPublic,
+    rawHashType,
+    rawCredHashType
+  );
 
   if (output === undefined) {
     console.log(`${JSON.stringify(vc)}`);
@@ -99,10 +75,8 @@ export const issueVC = async (
     fs.writeFileSync(output, JSON.stringify(vc));
   }
 
-  // step 6: encrypt message and send it to server
+  // step 3: encrypt message and send it to server
   const message = await encryptMessage('Send_issuedVC', vc, attester, claimer.getKeyUrl('keyAgreement'));
 
   await sendEncryptedMessage(baseUrl, message);
-
-  //   return 1;
 };
--- packages/cli/src/utils.ts
+++ packages/cli/src/utils.ts
@@ -7,8 +7,10 @@ import qs from 'qs';
 
 import { CType } from '@zcloak/ctype/types';
 import { Did, keys } from '@zcloak/did';
-import { DidDocument } from '@zcloak/did-resolver/types';
+import { DidDocument, DidUrl } from '@zcloak/did-resolver/types';
 import { Keyring } from '@zcloak/keyring';
+import { Raw, VerifiableCredentialBuilder } from '@zcloak/vc';
+import { HashType, RawCredential, VerifiableCredential } from '@zcloak/vc/types';
 
 export async function passwordPrompt(description: string, validate?: (input: string) => boolean) {
   const passwordPrompt = await inquirer.prompt<{ password: string }>([
@@ -66,5 +68,47 @@ export async function sendEncryptedMessage(baseUrl: string, message: any) {
     await axios.post(`${baseUrl}/message`, message);
   } catch (err) {
     console.error(err);
+  }
+}
+
+export async function createVC(
+  baseUrl: string,
+  attester: Did,
+  claimerDid: string,
+  ctypeHash: string,
+  content: string,
+  isPublic: number,
+  rawHashType = 'Blake3',
+  rawCredHashType = 'Keccak256'
+): Promise<VerifiableCredential<false> | VerifiableCredential<true>> {
+  // step 1: get ctype
+  const ctype: CType = await getCType(baseUrl, ctypeHash);
+
+  // step 2: build raw
+  const claimerDidUrl = claimerDid as DidUrl;
+  const realRawHashType = rawHashType as HashType;
+  const raw = new Raw({
+    contents: JSON.parse(content),
+    owner: claimerDidUrl,
+    ctype,
+    hashType: realRawHashType
+  });
+
+  // step 3: build rawCredential from raw
+  const realRawCredHashType = rawCredHashType as HashType;
+  const rawCredential: RawCredential = raw.toRawCredential(realRawCredHashType);
+
+  // step 4: build a vcBuilder by using rawCredential and ctype
+  const vcBuilder = VerifiableCredentialBuilder.fromRawCredential(rawCredential, ctype)
+    .setExpirationDate(null)
+    .setIssuanceDate(Date.now());
+
+  // step5: build a vc
+  if (isPublic === 0) {
+    return await vcBuilder.build(attester, false);
+  } else if (isPublic === 1) {
+    return await vcBuilder.build(attester, true);
+  } else {
+    throw new Error('invalid is-public flag value');
   }
 }

27115837014c433c60ad428ff5ed870697bd46ff
vsszhang <vsszhang@gmail.com>
2023-04-14 09:56:10 +0800

    refactor: use utils method

--- packages/cli/src/commands/queryCType.ts
+++ packages/cli/src/commands/queryCType.ts
@@ -1,11 +1,11 @@
 // Copyright 2021-2023 zcloak authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import axios from 'axios';
 import fs from 'fs';
-import qs from 'qs';
 
 import { CType } from '@zcloak/ctype/types';
+
+import { getCType } from '../utils';
 
 export const queryCType = async (env: string, ctypeHash: string, output?: string) => {
   let baseUrl: string;
@@ -26,13 +26,7 @@ export const queryCType = async (env: string, ctypeHash: string, output?: string
     return;
   }
 
-  const ctypeRes = await axios.get(`${baseUrl}/ctype?${qs.stringify({ id: ctypeHash })}`);
-
-  if (ctypeRes.status !== 200) {
-    throw new Error(`ctype query failed ${ctypeHash}`);
-  }
-
-  const ctype: CType = ctypeRes.data.data[0].rawData;
+  const ctype: CType = await getCType(baseUrl, ctypeHash);
 
   if (output === undefined) {
     console.log(`${JSON.stringify(ctype)}`);
--- packages/cli/src/commands/queryDidDoc.ts
+++ packages/cli/src/commands/queryDidDoc.ts
@@ -1,8 +1,9 @@
 // Copyright 2021-2023 zcloak authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import axios from 'axios';
 import fs from 'fs';
+
+import { getDidDoc } from '../utils';
 
 export const queryDidDoc = async (env: string, didUrl: string, output?: string) => {
   let baseUrl: string;
@@ -17,16 +18,10 @@ export const queryDidDoc = async (env: string, didUrl: string, output?: string) 
     return;
   }
 
-  const didRes = await axios.get(`${baseUrl}/did/${didUrl}`);
-
-  if (didRes.status !== 200) {
-    throw new Error(`get ${didUrl} did document failed`);
-  }
-
-  const holderDidDoc = didRes.data.data.rawData;
+  const holderDidDoc = await getDidDoc(baseUrl, didUrl);
 
   if (output === undefined) {
-    console.log(`holder Did Doc: ${JSON.stringify(holderDidDoc)}`);
+    console.log(`${JSON.stringify(holderDidDoc)}`);
   } else {
     console.log(`output did object into: ${output}`);
     fs.writeFileSync(output, JSON.stringify(holderDidDoc));

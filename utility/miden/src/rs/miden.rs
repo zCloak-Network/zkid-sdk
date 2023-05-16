041b525fcc086e716148e285a773e41cada8514f
zc.zhang <zzcwoshizz1@gmail.com>
2023-05-16 12:33:19 +0800

    feat: update wasm deps

--- utility/miden/src/rs/miden.rs
+++ utility/miden/src/rs/miden.rs
@@ -1,7 +1,7 @@
 // Copyright 2021-2023 zcloak authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-use miden_vm::{execute_zk_program, generate_program_hash, verify_zk_program};
+use miden_vm::{execute_zk_program, generate_program_hash};
 use wasm_bindgen::prelude::*;
 
 /// miden execute zk program
@@ -14,10 +14,4 @@ pub fn ext_execute_zk_program(program_code: String, stack_init: String, advice_t
 #[wasm_bindgen]
 pub fn ext_generate_program_hash(program_in_assembly: String) -> String {
 	return generate_program_hash(program_in_assembly)
-}
-
-/// miden verify zk program result
-#[wasm_bindgen]
-pub fn ext_verify_zk_program(program_hash: String, stack_inputs: String, final_result: String) -> u32 {
-	return verify_zk_program(program_hash, stack_inputs, final_result)
 }

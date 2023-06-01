73218eff3d27dc574ed1b409c0dbd6a864537aec
zc.zhang <zzcwoshizz1@gmail.com>
2023-06-01 16:44:05 +0800

    fix: if there are multiple same subject values in `vc`, the nonces will be empty when parse `vc` to `miden_input`.

--- protocol/vc/src/parser.ts
+++ protocol/vc/src/parser.ts
@@ -1,12 +1,13 @@
 // Copyright 2021-2023 zcloak authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import { hexToU8a } from '@polkadot/util';
+import { hexToU8a, u8aToHex } from '@polkadot/util';
 
 import { rlpEncode } from '@zcloak/crypto';
 import { VerifiableCredential } from '@zcloak/vc/types';
 
 import { makeMerkleTree } from './rootHash';
+import { rlpEncode as rlpEncodeForRoothash } from './utils';
 
 // Indexes represent which leaf need to be parsed, and calculate in the VM
 // Here, [1,2,3,4,5] means 5 leaves need to be parsed.
@@ -24,9 +25,12 @@ export function toMidenInput(data: VerifiableCredential<false>, leaves: number[]
   const hashType = data.hasher[0];
   const contents = data.credentialSubject;
 
-  const claimNonceMap = data.credentialSubjectNonceMap;
-  const nonces = Object.values(claimNonceMap);
   const contentsData = Object.values(contents);
+  const nonces = contentsData.map((item) => {
+    const encoded = u8aToHex(rlpEncodeForRoothash(item, hashType));
+
+    return data.credentialSubjectNonceMap[encoded];
+  });
 
   const contentData = contentsData.map((c) => {
     const encoded = Array.from(rlpEncode(c));
